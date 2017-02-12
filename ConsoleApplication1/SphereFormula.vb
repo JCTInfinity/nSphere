a3b3f69da91af4d825e407fdfcbad2e8300cecec
Chris Tyler <jchristyler@gmail.com>
2017-02-12 10:50:05 -0600

    Renamed Pis πs, because I like it

--- ConsoleApplication1/SphereFormula.vb
+++ ConsoleApplication1/SphereFormula.vb
@@ -1,13 +1,13 @@
 ﻿Public Class SphereFormula
     Public Shared sCache As SphereFormula
     Public Property Dimensions As UInt16
-    Public Property Pis As UInt16 = 0
+    Public Property πs As UInt16 = 0
     Public Property Rs As UInt16
     Public Property Frac As fraction = 1
     Public Overloads Function ToString(ByVal FullFraction As Boolean) As String
         Dim factors As New List(Of String)
         If Frac <> 1 Then factors.Add(Frac.SimplestForm.ToString(FullFraction))
-        If Pis > 0 Then factors.Add(powerString("π", Pis))
+        If πs > 0 Then factors.Add(powerString("π", πs))
         factors.Add(powerString("r", Rs))
         Return String.Join(" * ", factors)
     End Function
@@ -23,7 +23,7 @@
         Rs = d
         If d > 0 Then
             Frac = sCache.Frac
-            Pis = d \ 2
+            πs = d \ 2
             If d Mod 2 <> 0 Then Frac *= 2
             f(d)
         End If
@@ -41,7 +41,7 @@
         Return Math.Exp(nVolumeExp(r))
     End Function
     Public Function nVolumeExp(ByVal r As Double) As Double
-        Return Frac.ExpValue + Math.Log(Math.PI) * Pis + Math.Log(r) * Rs
+        Return Frac.ExpValue + Math.Log(Math.PI) * πs + Math.Log(r) * Rs
     End Function
     Private Shared fCache As New Dictionary(Of Integer, fraction)
     Private Sub f(ByVal n As Integer)
